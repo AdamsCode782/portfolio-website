9109862c5d2229bcf919ad9e55931b716e0a75dc
Adam Steele <asteeledesign@gmail.com>
2026-04-21 16:57:31 -0400

    Clean up comments in Home component
    Removed comments for theme toggle, navbar, main content, and footer.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,11 @@ import { Footer } from "../components/Footer";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Theme Toggle */}
       {/* Background Effects */}
       <StarBackground />
 
-      {/* Navbar */}
       <Navbar />
-      {/* Main Content */}
+
       <main>
         <HeroSection />
         <AboutSection />
@@ -25,7 +23,6 @@ export const Home = () => {
         <ContactSection />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
